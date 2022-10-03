050e202940029b8d0001fa575b8fd0038a7d0109
Woojinger <tlsdnwls0511@naver.com>
2022-10-03 19:12:25 +0900

    feat: add protogen.sh and modify init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,8 @@
 set -x
-rm -rf ~/.iq-chain
+rm -rf ~/.interchain-queries
 
-MONIKER=iq-chain-test
-CHAINID=iq-chain-1
+MONIKER=interchain-queries-test
+CHAINID=interchain-queries-1
 KEYRING=test
 
 MNEMONIC_FILE1=mnemonic1.txt
@@ -12,39 +12,40 @@ MNEMONIC_FILE4=mnemonic4.txt
 MNEMONIC_FILE5=mnemonic5.txt
 MNEMONIC_FILE6=mnemonic6.txt
 
-iq-chaind init $MONIKER --chain-id $CHAINID
+interchain-queriesd init $MONIKER --chain-id $CHAINID
 
-iq-chaind config keyring-backend $KEYRING
+interchain-queriesd config keyring-backend $KEYRING
 
-iq-chaind keys add genkey --recover < $MNEMONIC_FILE1
-iq-chaind keys add relayer --recover < $MNEMONIC_FILE2
-iq-chaind keys add boosik --recover < $MNEMONIC_FILE3
-iq-chaind keys add sangyun --recover < $MNEMONIC_FILE4
-iq-chaind keys add soojin --recover < $MNEMONIC_FILE5
-iq-chaind keys add wujinger --recover < $MNEMONIC_FILE6
+interchain-queriesd keys add genkey --recover < $MNEMONIC_FILE1
+interchain-queriesd keys add relayer --recover < $MNEMONIC_FILE2
+interchain-queriesd keys add boosik --recover < $MNEMONIC_FILE3
+interchain-queriesd keys add sangyun --recover < $MNEMONIC_FILE4
+interchain-queriesd keys add soojin --recover < $MNEMONIC_FILE5
+interchain-queriesd keys add wujinger --recover < $MNEMONIC_FILE6
 
-iq-chaind add-genesis-account $(iq-chaind keys show genkey -a) 10000000000uiq
-iq-chaind add-genesis-account $(iq-chaind keys show relayer -a) 10000000000uiq
-iq-chaind add-genesis-account $(iq-chaind keys show boosik -a) 10000000000uiq
-iq-chaind add-genesis-account $(iq-chaind keys show sangyun -a) 10000000000uiq
-iq-chaind add-genesis-account $(iq-chaind keys show soojin -a) 10000000000uiq
-iq-chaind add-genesis-account $(iq-chaind keys show wujinger -a) 10000000000uiq
+interchain-queriesd add-genesis-account $(iq-chaind keys show genkey -a) 10000000000uiq
+interchain-queriesd add-genesis-account $(iq-chaind keys show relayer -a) 10000000000uiq
+interchain-queriesd add-genesis-account $(iq-chaind keys show boosik -a) 10000000000uiq
+interchain-queriesd add-genesis-account $(iq-chaind keys show sangyun -a) 10000000000uiq
+interchain-queriesd add-genesis-account $(iq-chaind keys show soojin -a) 10000000000uiq
+interchain-queriesd add-genesis-account $(iq-chaind keys show wujinger -a) 10000000000uiq
 
-iq-chaind gentx genkey 1000000000uiq --chain-id $CHAINID
+interchain-queriesd gentx genkey 1000000000uiq --chain-id $CHAINID
 
 
-iq-chaind collect-gentxs
+interchain-queriesd collect-gentxs
 
 # update staking genesis
-cat $HOME/.iq-chain/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="uiq"' > $HOME/.iq-chain/config/tmp_genesis.json && mv $HOME/.iq-chain/config/tmp_genesis.json $HOME/.iq-chain/config/genesis.json
-cat $HOME/.iq-chain/config/genesis.json | jq '.app_state["staking"]["params"]["max_entries"]="10"' > $HOME/.iq-chain/config/tmp_genesis.json && mv $HOME/.iq-chain/config/tmp_genesis.json $HOME/.iq-chain/config/genesis.json
+cat $HOME/.interchain-queries/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="uiq"' > $HOME/.interchain-queries/config/tmp_genesis.json && mv $HOME/.interchain-queries/config/tmp_genesis.json $HOME/.interchain-queries/config/genesis.json
+cat $HOME/.interchain-queries/config/genesis.json | jq '.app_state["staking"]["params"]["max_entries"]="10"' > $HOME/.interchain-queries/config/tmp_genesis.json && mv $HOME/.interchain-queries/config/tmp_genesis.json $HOME/.interchain-queries/config/genesis.json
 
 # update mint denom
-cat $HOME/.iq-chain/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="uiq"' > $HOME/.iq-chain/config/tmp_genesis.json && mv $HOME/.iq-chain/config/tmp_genesis.json $HOME/.iq-chain/config/genesis.json
+cat $HOME/.interchain-queries/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="uiq"' > $HOME/.interchain-queries/config/tmp_genesis.json && mv $HOME/.interchain-queries/config/tmp_genesis.json $HOME/.interchain-queries/config/genesis.json
 
 # update crisis variable to uiq
-cat $HOME/.iq-chain/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="uiq"' > $HOME/.iq-chain/config/tmp_genesis.json && mv $HOME/.iq-chain/config/tmp_genesis.json $HOME/.iq-chain/config/genesis.json
+cat $HOME/.interchain-queries/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="uiq"' > $HOME/.interchain-queries/config/tmp_genesis.json && mv $HOME/.interchain-queries/config/tmp_genesis.json $HOME/.interchain-queries/config/genesis.json
 
 # udpate gov genesis
-cat $HOME/.iq-chain/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="120s"' > $HOME/.iq-chain/config/tmp_genesis.json && mv $HOME/.iq-chain/config/tmp_genesis.json $HOME/.iq-chain/config/genesis.json
-cat $HOME/.iq-chain/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="uiq"' > $HOME/.iq-chain/config/tmp_genesis.json && mv $HOME/.iq-chain/config/tmp_genesis.json $HOME/.iq-chain/config/genesis.json
+cat $HOME/.interchain-queries/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="120s"' > $HOME/.interchain-queries/config/tmp_genesis.json && mv $HOME/.interchain-queries/config/tmp_genesis.json $HOME/.interchain-queries/config/genesis.json
+cat $HOME/.interchain-queries/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="uiq"' > $HOME/.interchain-queries/config/tmp_genesis.json && mv $HOME/.interchain-queries/config/tmp_genesis.json $HOME/.interchain-queries/config/genesis.json
+cat $HOME/.interchain-queries/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="uiq"' > $HOME/.interchain-queries/config/tmp_genesis.json && mv $HOME/.interchain-queries/config/tmp_genesis.json $HOME/.interchain-queries/config/genesis.json
--- x/ibc-query/types/query.pb.gw.go
+++ x/ibc-query/types/query.pb.gw.go
@@ -181,7 +181,7 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 }
 
 var (
-	pattern_Query_CrossChainQueryResult_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"ibc", "apps", "ibc-query", "v1", "id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_CrossChainQueryResult_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"ibc", "apps", "ibc-query", "v1", "id"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
 var (
