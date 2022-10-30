15f13b51e083b2ba91bd37f68a9f098ef57811da
sujine2 <twinsujin22@gmail.com>
2022-10-30 16:18:47 +0900

    test: Add test helper functions

--- testutil/keeper/unit_test_helpers.go
+++ testutil/keeper/unit_test_helpers.go
@@ -10,8 +10,10 @@ import (
 	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	typesparams "github.com/cosmos/cosmos-sdk/x/params/types"
+	clienttypes "github.com/cosmos/ibc-go/v5/modules/core/02-client/types"
 	ibcquerykeeper "github.com/cosmos/interchain-queries/x/ibc_query/keeper"
 	gomock "github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
@@ -101,4 +103,53 @@ func GetIBCQueryKeeperAndCtx(t *testing.T, params InMemKeeperParams) (
 	ctrl := gomock.NewController(t)
 	mocks := NewMockedKeepers(ctrl)
 	return NewInMemIBCQueryKeeper(params, mocks), params.Ctx, ctrl, mocks
+}
+
+// Returns an in-memory provider keeper, context, controller, and mocks after query data is stored in the store
+func GetSubmitCrossChainQueryKeeperAndCtx(IBCQueryKeeper ibcquerykeeper.Keeper, 
+	ctx sdk.Context, mocks MockedKeepers) (
+	ibcquerykeeper.Keeper, sdk.Context, MockedKeepers, *types.MsgSubmitCrossChainQueryResponse) {
+
+	var( errFromNewCap  error )
+	senderAddr := "cosmos37dtl0mjt3t77dpoyz2edqzjpszulwhgnga7ljs"
+	dummyCap := &capabilitytypes.Capability{}
+
+	msg := types.MsgSubmitCrossChainQuery{
+		Path:               "test/query_path",
+		LocalTimeoutHeight: clienttypes.NewHeight(0, uint64(ctx.BlockHeight() + 50)),
+		LocalTimeoutStamp:  uint64(ctx.BlockTime().UnixNano() + 5000000),
+		QueryHeight:        uint64(ctx.BlockHeight()),
+		ChainId:            "ibc-query",
+		Sender:             senderAddr,
+	}
+
+	nextQuerySeq := IBCQueryKeeper.GetNextQuerySequence(ctx)
+	queryID := types.FormatQueryIdentifier(nextQuerySeq)
+	gomock.InOrder(			
+		mocks.MockScopedKeeper.EXPECT().NewCapability(
+			ctx, types.FormatQueryCapabilityIdentifier(queryID, msg.Sender),
+		).Return(dummyCap, errFromNewCap),
+	)
+	query, _ := IBCQueryKeeper.SubmitCrossChainQuery(ctx, &msg)
+	return IBCQueryKeeper, ctx, mocks, query
+}
+
+
+// Returns an in-memory provider keeper, context, controller, and mocks after query result data is stored in the store
+func GetSubmitCrossChainQueryResultKeeperAndCtx(IBCQueryKeeper ibcquerykeeper.Keeper, 
+	ctx sdk.Context, mocks MockedKeepers) (
+	ibcquerykeeper.Keeper, sdk.Context, MockedKeepers, types.MsgSubmitCrossChainQueryResult) {
+
+	senderAddr := "cosmos37dtl0mjt3t77dpoyz2edqzjpszulwhgnga7ljs"
+	IBCQueryKeeper, ctx, mocks, query := GetSubmitCrossChainQueryKeeperAndCtx(IBCQueryKeeper, ctx, mocks)
+
+	queryResult := types.MsgSubmitCrossChainQueryResult{
+		Id:           query.Id,
+		QueryHeight:  uint64(ctx.BlockHeight()),
+		Result:       types.QueryResult_QUERY_RESULT_SUCCESS,
+		Data:         []byte("test result data"),
+		Sender:       senderAddr,
+	}
+	IBCQueryKeeper.SubmitCrossChainQueryResult(ctx, &queryResult)
+	return IBCQueryKeeper, ctx, mocks, queryResult
 }
